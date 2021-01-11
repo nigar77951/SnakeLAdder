f2b4d0bd4775eb6755a1ba431ccc9589ce3de9a2
unknown <nigar77951@gmail.com>
2021-01-11 19:49:39 +0530

    checking for no play, ladder, snake option

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,27 @@
 public class SnakeLadder {
+    public final static int IS_LADDER=1;
+    public static final int IS_SNAKE=2;
     public static void main(String[] args) {
-    int strtpos=0;
-    int curpos=0;
-        System.out.println("player starts to play at pos : "+ strtpos);
+        int strtpos=0;
+        int curpos=0;
+
         int dicenum = (int) ((Math.floor(Math.random() * 10 % 6)) + 1);
         System.out.println("Current face of die is : " + dicenum);
 
+        int play = (int) (Math.floor(Math.random() * 10) % 3);
+        System.out.println("option : " +play);
+        switch(play) {
+            case IS_LADDER:
+                curpos += dicenum;
+                break;
+            case IS_SNAKE:
+                curpos -= dicenum;
+                break;
+            default:
+                curpos = curpos;
+                break;
+        }
+        System.out.println("new position = "+curpos);
     }
 
 }
