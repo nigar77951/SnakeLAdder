b0af872827574cbd9c482a2ba6c481f574d8442a
unknown <nigar77951@gmail.com>
2021-01-11 19:37:32 +0530

    snake ladder with single player

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,7 @@
 public class SnakeLadder {
     public static void main(String[] args) {
-
-        System.out.println("...Snake and ladder simulator...");
+    int strtpos=0;
+    System.out.println("player starts to play at pos : "+ strtpos);
     }
 }
+
