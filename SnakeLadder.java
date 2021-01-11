e725e644f030eec8ce16a11ea293996708280878
unknown <nigar77951@gmail.com>
2021-01-11 19:43:09 +0530

    roll die to get no. btw 1-6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,11 @@
 public class SnakeLadder {
     public static void main(String[] args) {
     int strtpos=0;
-    System.out.println("player starts to play at pos : "+ strtpos);
-    }
-}
+    int curpos=0;
+        System.out.println("player starts to play at pos : "+ strtpos);
+        int dicenum = (int) ((Math.floor(Math.random() * 10 % 6)) + 1);
+        System.out.println("Current face of die is : " + dicenum);
 
+    }
+
+}
